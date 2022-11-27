5b2e9cfe3f306208fb28fed957d750219ae3a156
Joseph boyadjian <lena.boya26@gmail.com>
2022-11-27 20:36:04 +0100

    Responsive

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+
+
 window.addEventListener('load', () => {
     const form =document.querySelector('#new-task-form');
     const input = document.querySelector('#new-task-input');
     const list_el = document.querySelector('#tasks');
+
+    //localstorage
+   
    
     form.addEventListener('submit', (e) => {
         e.preventDefault()
